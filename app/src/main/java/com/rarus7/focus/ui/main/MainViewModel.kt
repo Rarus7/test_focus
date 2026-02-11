1032e5a396886972ea421a830af2c26f961f3ec4
Maksim Filimonov <fil_max@mail.ru>
2026-02-11 21:40:05 +0900

    Начинем с начала

--- app/src/main/java/com/rarus7/focus/ui/main/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.rarus7.focus.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/rarus7/focus/ui/registration/RegistrationViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.rarus7.focus.ui.registration
-
-import androidx.lifecycle.ViewModel
-
-class RegistrationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
